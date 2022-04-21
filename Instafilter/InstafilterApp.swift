8ff73304cec2ea46608b29ad50832e186a3f0a63
Tomasz Koba <tomaszkoba@outlook.com>
2022-04-21 22:47:28 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Tomasz Koba on 21/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
